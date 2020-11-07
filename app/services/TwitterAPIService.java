2e29bf7c873c87175b30678fe6e2bbcbc7ec24f6
griffin98 <griffin98@protonmail.com>
2020-11-07 20:11:38 +0530

    Revert "TwitterAPIService: correctly fetch 250 tweets"
    This reverts commit a27fff16c439a83b0be0ee85843a44dfb93a0c49.

--- app/services/TwitterAPIService.java
+++ app/services/TwitterAPIService.java
@@ -28,41 +28,26 @@ public class TwitterAPIService {
 
         ArrayList<Tweet> result = new ArrayList<>();
 
-        long lastID = Long.MAX_VALUE;
-
         Query query  = new Query(keyword);
+        try {
+            QueryResult queryResult = twitter.search(query);
+            List<Status> statuses = queryResult.getTweets();
 
-        while(result.size() < limit) {
+            int i=0;
+            for(Status status : statuses) {
+                User user = UserFactory.getInstance().getOrCreateUser(status.getUser().getId(), status.getUser().getName(),
+                        status.getUser().getScreenName(), status.getUser().getMiniProfileImageURL(), status.getUser().getName());
 
-            if(limit - result.size() > 100)
-                query.setCount(100);
-            else
-                query.setCount(limit - result.size());
+                Tweet tweet = new Tweet(user, status.getText(), status.getCreatedAt());
+                result.add(tweet);
 
-            try {
-                QueryResult queryResult = twitter.search(query);
-                List<Status> statuses = queryResult.getTweets();
-
-                for(Status status : statuses) {
-
-                    if(status.getId() < lastID)
-                        lastID = status.getId();
-
-                    User user = UserFactory.getInstance().getOrCreateUser(status.getUser().getId(), status.getUser().getName(),
-                            status.getUser().getScreenName(), status.getUser().getMiniProfileImageURL(), status.getUser().getName());
-
-                    Tweet tweet = new Tweet(user, status.getText(), status.getCreatedAt());
-                    result.add(tweet);
-
-                }
-
-            } catch (TwitterException e) {
-                e.printStackTrace();
-                return null;
+                if(++i == limit)
+                    break;
             }
 
-            query.setMaxId(lastID - 1);
-
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            return null;
         }
 
         return result;
